641cb2d98dd95647c41552afe85b4b26ffb46e12
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-13 09:00:29 +1200

    Added a glossary for the world id

--- Plants vs Zombies 2 sql code.py
+++ Plants vs Zombies 2 sql code.py
@@ -1,9 +1,21 @@
+'''This is code for a Plant's vs Zombies 2 code by Kallum'''
 import sqlite3
-
+print("1 = Ancient Egypt")
+print("2 = Pirate Seas")
+print("3 = Wild West")
+print("4 = Frostbite Caves")
+print("5 = Lost City")
+print("6 = Far Future")
+print("7 = Dark Ages")
+print("8 = Neon Mixtape Tour")
+print("9 = Jurassic Marsh")
+print("10 = Big Wave Beach")
+print("11 = Modern Day")
+print("12 = Player's House")
 DATABASE = 'PVZ2.db'
 
 def print_all_Plants():
-    World = input("What world unlocked: ")
+    World = input("What world id: ")
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
         sql = "SELECT Plant_Name, World_Unlocked_ID, Sun_Cost FROM Plants WHERE World_Unlocked_ID = ?;"
