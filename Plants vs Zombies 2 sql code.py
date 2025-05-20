f200ff2f106643aa54a207a29f372457c2c6783a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-20 12:40:33 +1200

    Copy and pasted the sun cost function and changed it to filter by plant name alphabeticaly

--- Plants vs Zombies 2 sql code.py
+++ Plants vs Zombies 2 sql code.py
@@ -19,12 +19,23 @@ def print_world_glossary():
     for id, name in worlds.items():
         print(f"{id:2} = {name}")
 
-# Functions that filters by suncost so cheapest goes firts
+# Function that filters by suncost so cheapest goes firts
 def get_plants_by_world_sorted_by_suncost(world_id):
     try:
         with sqlite3.connect(DATABASE) as db:
             cursor = db.cursor()
             sql = "SELECT Plant_Name, Sun_Cost FROM Plants WHERE World_Unlocked_ID = ? ORDER BY Sun_Cost ASC;"
+            cursor.execute(sql, (world_id,))
+            return cursor.fetchall()
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return []
+  # Function that orders by plant name alphabetically   
+def get_plants_by_world_sorted_by_name(world_id):
+    try:
+        with sqlite3.connect(DATABASE) as db:
+            cursor = db.cursor()
+            sql = "SELECT Plant_Name, Sun_Cost FROM Plants WHERE World_Unlocked_ID = ? ORDER BY Plant_Name ASC;"
             cursor.execute(sql, (world_id,))
             return cursor.fetchall()
     except sqlite3.Error as e:
